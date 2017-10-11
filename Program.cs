c3908db2521fcf4d359548d552f7d7cb2fa307e2
FIC <lqa.gabriel@gmail.com>
2017-10-11 19:02:20 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace aula1110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("./perguntas.txt"))
+            {
+                Console.WriteLine("Arquivo 'perguntas' não existe.");
+            }
+            string[] strperguntas = 
+        }
+    }
+}
